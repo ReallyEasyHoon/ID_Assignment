0779189818929a521883a88586db51721c2e1ffa
AngMingZe <angmingze21@gmail.com>
2022-02-11 15:06:54 +0800

    weather api created and works(need to beautify)

--- weatherScript.js
+++ weatherScript.js
@@ -1,29 +1,41 @@
 const api = {
     key: "809c983b54fedce7b1495ad4ec8790d0",
     base: "https://api.openweathermap.org/data/2.5/"
-  }
+}
   
-  function getResults() {
-    fetch(`${api.base}weather?q=${Singapore}&units=metric&APPID=${api.key}`)
-      .then(weather => {
-        return weather.json();
-      }).then(displayResults);
-  }
+const runner = document.querySelector('.W-api');
+runner.addEventListener(getResults('Singapore'));
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
   
-  function displayResults (weather) {
-    let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
-  
+function displayResults (weather) {
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
-  
+
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-  
+
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
-  
+
     let hilow = document.querySelector('.hi-low');
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-  }
+}
+  
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`;
+}
