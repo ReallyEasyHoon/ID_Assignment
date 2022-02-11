2fdc50663d4575b60d2b77fa36950f5e22c41752
AngMingZe <angmingze21@gmail.com>
2022-02-11 11:06:22 +0800

    Weather api part 1

---
+++ weatherScript.js
@@ -0,0 +1,29 @@
+const api = {
+    key: "809c983b54fedce7b1495ad4ec8790d0",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  function getResults() {
+    fetch(`${api.base}weather?q=${Singapore}&units=metric&APPID=${api.key}`)
+      .then(weather => {
+        return weather.json();
+      }).then(displayResults);
+  }
+  
+  function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+  
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+  
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  }
